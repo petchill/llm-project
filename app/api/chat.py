07f0cfa2fac0ac30b97988ce830cd30746a565eb
petchill <songpon111333@gmail.com>
2025-07-20 09:10:37 +1000

    feat: add message history to chain

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, jsonify
 
 from app.core.langchain import llm_model
+from app.core.langchain import init_chain
 
 
 def PostChat():
     payload = request.get_json()
+    session_id = payload['session_id']
     message = payload['message']
-    print(message)
-    response = llm_model.predict(message)
+    config = {"configurable": {"session_id": session_id}}
+    chain = init_chain()
+    response = chain.invoke({"question": message}, config=config)
+    print(response)
     return jsonify({
-        "message": response
+        "message": response.content
     })
--- app/core/langchain.py
+++ app/core/langchain.py
@@ -1,6 +1,10 @@
 import os
 import getpass
 from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_mongodb import MongoDBChatMessageHistory
+from langchain_openai import ChatOpenAI
 
 
 def init_model():
@@ -14,3 +18,31 @@ def init_model():
 
 
 llm_model = init_model()
+
+mongo_conn_string = os.getenv("MONGODB_URI")
+mongo_database = os.getenv("MONGODB_DATABASE")
+
+
+def init_chain():
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful assistant."),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{question}"),
+        ]
+    )
+    chain = prompt | ChatOpenAI()
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        lambda session_id: MongoDBChatMessageHistory(
+            session_id=session_id,
+            connection_string=mongo_conn_string,
+            database_name=mongo_database,
+            collection_name="chat_histories",
+        ),
+        input_messages_key="question",
+        history_messages_key="history",
+    )
+
+    return chain_with_history
